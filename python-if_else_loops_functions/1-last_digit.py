5f866e828c06d264e8b5e6c3e898e528f46f0c83
Nelly <nelly@Nellys.localdomain>
2024-05-08 15:29:18 -0400

    Fixing uppercase on 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number > 0 or last_digit == 0:
-    print(f"Last Digit of {number} is {last_digit} ", end="")
+    print(f"Last digit of {number} is {last_digit} ", end="")
 else:
     print(f"Last digit of {number} is -{last_digit} ", end="")
 if last_digit > 5 and number > 0:
