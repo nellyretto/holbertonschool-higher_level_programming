5104cd9a340901fe77ddeeb1635235ddfcf41eb7
nellyretto <nellyretto@gmail.com>
2024-05-10 16:43:08 -0400

    Fixing output point for 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def main():
 
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("0 arguments:.")
+        print("0 arguments.")
     else:
         print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:", end='\n\n')
         for i in range(1, len(sys.argv)):
