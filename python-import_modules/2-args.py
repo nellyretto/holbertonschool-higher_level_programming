0cca3bf519483ee6e5d821cbff1c1b65ca63ef2c
nellyretto <nellyretto@gmail.com>
2024-05-10 16:38:46 -0400

    Updating output and new line for 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ def main():
     if num_args == 0:
         print("0 argument(s): .")
     else:
-        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:", end='\n\n')
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:", end='\n')
         for i in range(1, len(sys.argv)):
             print(f"{i}: {sys.argv[i]}")
 
