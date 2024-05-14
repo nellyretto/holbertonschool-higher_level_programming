2543011870a59517196f88a884ac7475b6c78a7b
nellyretto <nellyretto@gmail.com>
2024-05-13 21:40:37 -0400

    Adding 10-main.py

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    if not my_list:
+        return []
+    
+    result = []
+    for num in my_list:
+        result.append(num % 2 == 0)
+    
+    return result
