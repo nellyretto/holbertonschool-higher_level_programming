8349553dbc4d2a547528fb8c68e8ca84d52f2a0a
nellyretto <nellyretto@gmail.com>
2024-05-14 19:38:24 -0400

    Fixing mixed mu;tiplcation 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,6 +5,6 @@ def multiply_by_2(a_dictionary):
 
     new_dict = {}
     for key, value in a_dictionary.items():
-        new_dict[value] = key * 2
+        new_dict[key] = value * 2
     return new_dict   
-    
+
