7d7b6177e3a82688edf9d9db71c73d3e3867861f
nellyretto <nellyretto@gmail.com>
2024-05-14 19:33:22 -0400

    Testing 9th task

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,5 +5,5 @@ def multiply_by_2(a_dictionary):
 
     new_dict = {}
     for key, value in a_dictionary.items():
-        new_dict[key] = value * 2
+        new_dict[value] = key * 2
     return new_dict
