041c92ffd2b870bbd5c8c8e52ac65cea8132ef08
nellyretto <nellyretto@gmail.com>
2024-05-14 14:49:28 -0400

    Testing error for 1-search_replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+
+    for num in my_list:
+
+        if search in my_list == 2:
+            return replace
+
+        else:
+            return my_list
