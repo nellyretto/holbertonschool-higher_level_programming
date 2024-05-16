8daeb3d08113b9bc0abc0c5d62e76038f811864a
nellyretto <nellyretto@gmail.com>
2024-05-16 18:38:50 -0400

    Adding 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module defines a class Square.  """
+
+
+class Square:
+    """A simple class named Square"""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
