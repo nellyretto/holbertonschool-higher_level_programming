f229161b0f4f4a4e065340bec9bf38646d67529c
nellyretto <nellyretto@gmail.com>
2024-05-16 16:30:24 -0400

    Adding and testing 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
+
     result = []
     try:
         for i in range(list_length):
@@ -9,17 +10,19 @@ def list_division(my_list_1, my_list_2, list_length):
                 num1 = my_list_1[i] if i < len(my_list_1) else 0
                 num2 = my_list_2[i] if i < len(my_list_2) else 0
 
-                if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+                if isinstance(num1, (int, float)) and \
+                        isinstance(num2, (int, float)):
                     try:
                         result.append(num1 / num2)
                     except ZeroDivisionError:
                         result.append(0)
                         print("division by 0")
                 else:
-                    result.append(0)
                     print("wrong type")
+                    result.append(0)
             except IndexError:
                 print("out of range")
                 break
     finally:
+        print("out of range")
         return result
