0b0bb692151eb5ba85ac0d5e5fdee32aa80a22c8
nellyretto <nellyretto@gmail.com>
2024-05-15 20:43:22 -0400

    Adding formatting for 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for num in range(x):
-            print(my_list[num], end="")
+            print("{:d}".format(my_list[num]), end="")
             printed += 1
     except IndexError:
         print()
