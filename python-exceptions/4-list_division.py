a0223ba97b6563ff45b52c12be24234b6d05e689
nellyretto <nellyretto@gmail.com>
2024-05-16 16:33:31 -0400

    Fixing double output for 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,8 +7,8 @@ def list_division(my_list_1, my_list_2, list_length):
     try:
         for i in range(list_length):
             try:
-                num1 = my_list_1[i] if i < len(my_list_1) else 0
-                num2 = my_list_2[i] if i < len(my_list_2) else 0
+                num1 = my_list_1[i] if i < len(my_list_1) else None
+                num2 = my_list_2[i] if i < len(my_list_2) else None
 
                 if isinstance(num1, (int, float)) and \
                         isinstance(num2, (int, float)):
