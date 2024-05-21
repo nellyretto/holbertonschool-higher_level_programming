79c94caa8be7a7b44e4c6e4c076d0d6c491f5676
nellyretto <nellyretto@gmail.com>
2024-05-20 22:54:35 -0400

    Fixing condition for wrong output

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,5 +17,8 @@ def text_indentation(text):
     None
     """
 
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
     print(text.replace(". ", ".\n\n").replace("? ", "?\n\n"
                                               ).replace(": ", ":\n\n"))
