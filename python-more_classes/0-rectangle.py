b1b966b6a3e90c0da6f4bb1bf7d0e1f67ebf9458
nellyretto <nellyretto@gmail.com>
2024-05-21 12:18:22 -0400

    adding README and first task 0-rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""
+This module contains a class for representing squares.
+    """
+
+
+class Rectangle:
+
+    """
+    A class representing a square.
+
+    """
+
+
+pass
