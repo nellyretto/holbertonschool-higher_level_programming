af846e0ffe97f6195911b02f08e8a6f709a24f41
nellyretto <nellyretto@gmail.com>
2024-05-22 16:43:27 -0400

    Adding 5-base

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+
+    """
+    This is the base class for geometry objects.
+    """
+
+    pass
+
+
+"""
+This is the end of the BaseGeometry class definition.
+"""
