fa573895c1b585c4da249a6fac3af71a37dd4eae
nellyretto <nellyretto@gmail.com>
2024-05-22 12:01:03 -0400

    Adding final 0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -10,4 +10,4 @@ def lookup(obj):
     This function takes an object as input and returns a list of all
     the available attributes and methods of that object.
     """
-    return(dir(obj))
+    return dir(obj)
