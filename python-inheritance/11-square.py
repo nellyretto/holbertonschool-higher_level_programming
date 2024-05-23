8300417812ca98ae7793135708153571be766b16
nellyretto <nellyretto@gmail.com>
2024-05-22 23:56:51 -0400

    Validation for 11-square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -27,6 +27,7 @@ class Square(Rectangle):
         Args:
             size (int): The size of the square.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
