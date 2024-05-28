9a27525ebc71af4bef26aa525b3edec0b01f15e4
nellyretto <nellyretto@gmail.com>
2024-05-28 14:08:59 -0400

    Adding 5-save

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module provides a function to save an object to a JSON file.
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Saves the given object to a JSON file.
+
+    Args:
+        my_obj: The object to be saved.
+        filename: The name of the file to save the object to.
+
+    Returns:
+        None
+    """
+    with open(filename, 'w') as file:
+        file.write(filename)
+        return json.dumps(my_obj)
