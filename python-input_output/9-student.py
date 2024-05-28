09dc6805ee0e6563bf25fb7849172b1c571394ec
nellyretto <nellyretto@gmail.com>
2024-05-28 16:59:40 -0400

    Adding 9-student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Student class.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the Student object.
+
+        Returns:
+            dict: A dictionary containing the attributes of the Student object.
+        """
+        return self.__dict__
