660b6da840090f0af6486962fb42ad05a6d3bc78
nellyretto <nellyretto@gmail.com>
2024-05-28 15:23:33 -0400

    Adding 6-load

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import json
+
+"""
+This module provides a function to load data from a JSON file.
+"""
+
+
+def load_from_json_file(filename):
+    """
+    Load data from a JSON file and return the deserialized object.
+
+    Args:
+        filename (str): The path to the JSON file.
+
+    Returns:
+        object: The deserialized object.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        JSONDecodeError: If the file is not valid JSON.
+
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
