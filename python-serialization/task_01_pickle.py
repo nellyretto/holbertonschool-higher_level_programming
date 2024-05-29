4735e8e679e217a09efd8dccc25fb6fc04c173b8
nellyretto <nellyretto@gmail.com>
2024-05-29 15:20:44 -0400

    Adding task_01

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print('Name:', self.name)
+        print('Age:', self.age)
+        print('Is Student:', self.is_student)
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except (IOError, pickle.PickleError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                obj = pickle.load(file)
+                if isinstance(obj, cls):
+                    return obj
+                else:
+                    return None
+        except(IOError, pickle.PickleError):
+            return None
