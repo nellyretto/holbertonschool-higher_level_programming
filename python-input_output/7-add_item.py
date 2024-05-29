bfd046c82fe997b67fa7548975df35871cbb6bc1
nellyretto <nellyretto@gmail.com>
2024-05-28 23:10:36 -0400

    Changin 7-add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,14 +7,20 @@ list and saves them to a file in JSON format.
 Usage: ./7-add_item.py [arg1] [arg2] ...
 """
 import json
+import os.path
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from sys import argv
 
-try:
-    my_list = load_from_json_file('add_item.json')
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, 'add_item.json')
-except Exception:
-    my_list = sys.argv[1:]
-    save_to_json_file(my_list, 'add_item.json')
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+json_list = []
+
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
+
+for index in argv[1:]:
+    json_list.append(index)
+
+save_to_json_file(json_list, filename)
