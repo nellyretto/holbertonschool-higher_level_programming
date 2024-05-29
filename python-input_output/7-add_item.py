c2f89443ec9e9f2789c7c688911e0b4a13ead490
nellyretto <nellyretto@gmail.com>
2024-05-28 23:06:08 -0400

    trying 7 - add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
+
+"""
+Script: 7-add_item.py
+Description: This script adds command-line arguments to a Python
+list and saves them to a file in JSON format.
+Usage: ./7-add_item.py [arg1] [arg2] ...
+"""
+import json
 import sys
-from os import path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-filename = "add_item.json"
 
-def add_item_to_json():
-    """
-    This script adds command line arguments to a JSON file.
-
-    It first checks if the JSON file exists. If it does, it loads the contents of the file into a list.
-    If the file does not exist, it initializes an empty list.
-
-    It then extends the list with the command line arguments passed to the script.
-
-    Finally, it saves the updated list to the JSON file.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
+try:
+    my_list = load_from_json_file('add_item.json')
     my_list.extend(sys.argv[1:])
-
-    save_to_json_file(my_list, filename)
-
-add_item_to_json()
+    save_to_json_file(my_list, 'add_item.json')
+except Exception:
+    my_list = sys.argv[1:]
+    save_to_json_file(my_list, 'add_item.json')
