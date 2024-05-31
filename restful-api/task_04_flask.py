faaa68a6c19db16914fda0bc9200a99354c45bf7
nellyretto <nellyretto@gmail.com>
2024-05-31 15:53:35 -0400

    addig 04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -35,7 +35,7 @@ def add_user():
     username = new_user.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
-    if username == users:
+    if username in users:
         return jsonify({"error": "User already exists"}), 400
     users[username] = new_user
     return jsonify({"message": "User added", "user": new_user}), 201
