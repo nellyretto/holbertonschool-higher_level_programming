1395cfae59b52005a631d50d8ce5be0e9afe432d
nellyretto <nellyretto@gmail.com>
2024-05-31 12:20:33 -0400

    fixing API testing task_03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,16 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-
 class Server(BaseHTTPRequestHandler):
 
-    def _set_headers(self, content_type='text/html'):
-        self.send_response(200)
+    def _set_headers(self, content_type='text/html', status_code=200):
+        self.send_response(status_code)
         self.send_header('Content-type', content_type)
         self.end_headers()
 
     def do_GET(self):
         if self.path == '/':
+            self._set_headers()
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
             data = {"name": "John", "age": 30, "city": "New York"}
@@ -19,18 +19,19 @@ class Server(BaseHTTPRequestHandler):
         elif self.path == '/status':
             self._set_headers()
             self.wfile.write(b'OK')
+        elif self.path == '/info':
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self._set_headers(content_type='application/json')
+            self.wfile.write(json.dumps(info).encode('utf-8'))
         else:
-            self._set_headers()
+            self._set_headers(status_code=404)
             self.wfile.write(b'Endpoint not found')
 
-
-def run(server_class=HTTPServer, handler_class=Server,
-        port=8000):
+def run(server_class=HTTPServer, handler_class=Server, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
-
 
 if __name__ == "__main__":
     run()
