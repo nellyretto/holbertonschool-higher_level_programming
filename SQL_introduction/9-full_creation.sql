5d8caa72f2be98c8b04a6355f43419ac2ca08bba
nellyretto <nellyretto@gmail.com>
2024-06-17 18:39:30 -0400

    adding task 09

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creating a second table and creating multiple rows
+CREATE TABLE second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "Geroge", 8);
