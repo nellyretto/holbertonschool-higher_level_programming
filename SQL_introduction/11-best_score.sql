83954ffe3f3872bc88532affa2610910a5288af6
nellyretto <nellyretto@gmail.com>
2024-06-17 19:05:16 -0400

    fixing order for task 11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,8 @@
 -- list all records with score higher or equal than 10
-SELECT * FROM second_table
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
