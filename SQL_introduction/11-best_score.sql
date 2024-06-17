effe644712821aef194969ca7289815bcbf636c0
nellyretto <nellyretto@gmail.com>
2024-06-17 19:06:52 -0400

    11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,8 +1,4 @@
 -- list all records with score higher or equal than 10
 SELECT score, name FROM second_table
 WHERE score >= 10
-ORDER BY score DESC;SELECT score, name FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;SELECT score, name FROM second_table
-WHERE score >= 10
 ORDER BY score DESC;
