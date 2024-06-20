ebe11b7af0fa8c923a9f490afa24870981db63ff
nellyretto <nellyretto@gmail.com>
2024-06-20 14:40:46 -0400

    adding task 01

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Connects to a MySQL database and retrieves all
+    states whose name starts with 'N'.
+    Prints the retrieved rows.
+    """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
