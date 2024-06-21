9d00ac5ed8e2dfd3817e077431e5b9a8f08c9ee1
nellyretto <nellyretto@gmail.com>
2024-06-21 09:58:29 -0400

    Adding task 02

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+    Connects to a MySQL database and retrieves all rows from the 'states' table
+    where the name matches the provided name parameter.
+
+    Args:
+        name (str): The name to filter the states by.
+
+    Returns:
+        None
+    """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY "
+        "states.id ASC".format(sys.argv[4],))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
