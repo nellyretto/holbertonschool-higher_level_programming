2f77105d7a40a542c6a4b56ac3b4d52711f9eed3
nellyretto <nellyretto@gmail.com>
2024-06-21 18:49:57 -0400

    adding task 10 only searching and printing

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+"""
+    This script retrieves a State object from the database
+    based on the given state name.
+    If the state is found, it prints the state's id and name.
+    Otherwise, it prints "Not found".
+    """
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Get the state name from the command line arguments
+    search_state_name = sys.argv[4]
+
+    # Query the database for the state with the given name
+    state = session.query(State).filter(State.name ==
+                                        search_state_name).first()
+
+    # Print the state's id and name if found, otherwise print "Not found"
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
