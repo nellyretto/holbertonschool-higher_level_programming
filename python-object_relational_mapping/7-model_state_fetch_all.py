d0ca98085faa3f63ddffa25e933a8448ab4318cd
nellyretto <nellyretto@gmail.com>
2024-06-21 17:13:51 -0400

    refactor: Remove unnecessary code for creating model_state table
    This commit removes the unnecessary code for creating the model_state table in the database. The table creation is now handled by the `model_state` module, simplifying the code and improving maintainability.

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    Fetches all states from the database and prints their IDs and names.
+    """
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
