0b2b9486a50a307e2986d153cce1b2f3d3012357
nellyretto <nellyretto@gmail.com>
2024-06-21 19:58:12 -0400

    refactor: Simplify code for inserting a new state into the database

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+"""
+This script inserts a new state into the database using
+SQLAlchemy.
+
+The script takes three command-line arguments:
+- sys.argv[1]: MySQL username
+- sys.argv[2]: MySQL password
+- sys.argv[3]: MySQL database name
+
+The script performs the following steps:
+1. Creates a SQLAlchemy engine using the provided MySQL
+credentials.
+2. Creates the necessary tables in the database using
+the Base.metadata.create_all() method.
+3. Creates a session using sessionmaker and binds it to
+the engine.
+4. Creates a new State object with the name 'Louisiana'.
+5. Adds the new State object to the session.
+6. Commits the changes to the database.
+7. Queries all the states from the database and prints
+their IDs and names.
+8. Closes the session.
+
+Note: This script assumes that the 'model_state' module is
+imported and contains the necessary SQLAlchemy models.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    c1 = State(name='Louisiana')
+    session.add(c1)
+    session.commit()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
