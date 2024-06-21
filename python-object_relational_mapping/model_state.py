2738d3cf6168afdbfdf4549f5d63ff682c2c094d
nellyretto <nellyretto@gmail.com>
2024-06-21 16:20:35 -0400

    feat: Add model for states in database

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""
+    Represents a state in the database.
+
+    Attributes:
+        id (int): The unique identifier of the state.
+        name (str): The name of the state.
+    """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __tablename__ = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128),
+                  nullable=False)
