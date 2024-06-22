afb7911f27bc714fe6e80f8a5b441eeafb3841c5
nellyretto <nellyretto@gmail.com>
2024-06-21 23:37:36 -0400

    refactor: Update query to fetch cities along with their corresponding states

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,26 +3,15 @@
 """
 Module for task 14
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
-
-Base = declarative_base()
+from model_state import Base, State  # Import State here
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    """
-    City class represents a city in the database.
-
-    Attributes:
-        id (int): The unique identifier for the city.
-        name (str): The name of the city.
-    """
-
     __tablename__ = 'cities'
-    id = Column(Integer,
-                primary_key=True,
-                autoincrement=True,
-                nullable=False)
-    name = Column(String(128),
-                  nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
