a83ea324e6298bfd647145b7236f4d38f43fcb7e
nellyretto <danielys2416@gmail.com>
2024-07-15 22:58:42 -0400

    testing functionality

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
-import os 
-
+import os
 
 def get_invitations(template, attendees):
-
-    if not isinstance (template, str):
-        raise TypeError("Must be a string")
+    if not isinstance(template, str):
+        raise TypeError("Template must be a string")
     
-    if not isinstance (attendees, list):
-         if not all(isinstance(item, dict) for item in attendees):
-             raise TypeError("Must be a list of dictionaries")
+    if not isinstance(attendees, list) or not all(isinstance(item, dict) for item in attendees):
+        raise TypeError("Attendees must be a list of dictionaries")
          
     if not template:
         raise ValueError("Template is empty")
     
     if not attendees:
-        raise ValueError("Attendees is empty")
+        raise ValueError("Attendees list is empty")
     
-
     for index, attendee in enumerate(attendees, start=1):
-
         modified_template = template.replace("{name}", attendee.get('name', 'N/A'))
         modified_template = modified_template.replace("{event_title}", attendee.get('event_title', 'N/A'))
         modified_template = modified_template.replace("{event_date}", attendee.get('event_date', 'N/A'))
