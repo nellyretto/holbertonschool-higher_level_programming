0a4136c251e2ec52db8d853b4d6e9c8c7930df8c
nellyretto <danielys2416@gmail.com>
2024-07-26 16:08:34 -0400

    “Adding and testing functionality for task 3”

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("toggle_header");
+button.addEventListener("click",
+    function() {
+        if (document.querySelector("header").classList.contains('green')) {
+            document.querySelector("header").classList.replace('green', 'red');
+        } else {
+            document.querySelector("header").classList.replace('red', 'green')
+            }
+        }
+    );
