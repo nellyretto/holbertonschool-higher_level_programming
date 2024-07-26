9fdfce36cff08be7d7936e42db4df9ccae71af11
nellyretto <danielys2416@gmail.com>
2024-07-26 15:27:35 -0400

    “Adding and testing functionality for task 2”

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("red_header");
+button.addEventListener("click",
+    function() {
+        const header = document.querySelector("header");
+        header.classList.add("red");
+
+    });
